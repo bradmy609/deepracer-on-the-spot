115bd6adb7f5c863827b7ee33abcb4a1bd9df476
bradmy609 <bradmyers555@gmail.com>
2024-10-01 10:29:49 -0400

    average-delta-p

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -579,7 +579,8 @@ class Reward:
             # Progress component
             if prev_waypoint_index >= 18 and prev_waypoint_index <= 21:
                 reward += 3 * ((distance_reward * DISTANCE_MULTIPLE) + (speed_reward * SPEED_MULTIPLE))
-            reward += ((avg_delta_p + avg_delta_p2 + avg_delta_p4 + avg_delta_p8 + avg_delta_p16 + avg_delta_p32 + avg_delta_p64) * (1 + distance_reward)) + (distance_reward * DISTANCE_MULTIPLE) + (speed_reward * SPEED_MULTIPLE)
+            else:
+                reward += ((avg_delta_p + avg_delta_p2 + avg_delta_p4 + avg_delta_p8 + avg_delta_p16 + avg_delta_p32 + avg_delta_p64) * (1 + distance_reward)) + (distance_reward * DISTANCE_MULTIPLE) + (speed_reward * SPEED_MULTIPLE)
             
             if prev_waypoint_index >= 18 and prev_waypoint_index <= 27:
                 if speed > 2.5:
