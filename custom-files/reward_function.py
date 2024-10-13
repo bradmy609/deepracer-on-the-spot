9abf15fc55dc6821ca1cecdfc7553677b3b46d5f
bradmy609 <bradmyers555@gmail.com>
2024-10-13 18:40:24 -0400

    v44

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -794,7 +794,7 @@ class Reward:
                 delta_p8 = 4.5
                 
             avg_delta_p = (((delta_p1 * 2) + delta_p2 + delta_p3 + delta_p4 + delta_p5 + delta_p6) / 6)
-            cubed_avg_delta_p = ((delta_p_multiple) ** 3)
+            cubed_avg_delta_p = ((avg_delta_p * delta_p_multiple) ** 3)
             avg_delta_p_reward = (cubed_avg_delta_p)
             
             try:
